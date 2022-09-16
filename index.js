16de0ddcbc7dd2699b2ff953f865aefd785f028d
DEV <dassamiran05@gmail.com>
2022-09-16 14:28:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+// For database connection
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qs7t4ng.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+
+        const database = client.db("doctor_portal_13");
+        const serviceCollection = database.collection("services");
+
+        //get services
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World! from Doctor portal')
+})
+
+app.listen(port, () => {
+    console.log(`Doctor's app listening on port ${port}`);
+    //   console.log(process.env);
+})
