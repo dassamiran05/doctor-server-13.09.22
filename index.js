48aefc2a4e79a4eab444f59e5c018a81f65dc9cd
Samiran Das <dassamiran05@gmail.com>
2022-09-19 17:33:18 +0530

    All updated api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const {
     MongoClient,
     ServerApiVersion
 } = require('mongodb');
+const { emit } = require('nodemon');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -25,6 +27,24 @@ const client = new MongoClient(uri, {
     serverApi: ServerApiVersion.v1
 });
 
+function verifyJWT(req, res, next){
+    const authheader = req.headers.authorization;
+    if(!authheader){
+        return res.status(401).send({message:'Unauthorized Access'});
+    }
+    const token = authheader.split(' ')[1];
+    // console.log(token);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+        if(err){
+            return res.status(403).send({message:'Forbidden Access'});
+        }
+        req.decoded = decoded;
+        // console.log(decoded);
+        next();
+    })
+}
+
 async function run() {
     try {
         await client.connect();
@@ -32,6 +52,7 @@ async function run() {
         const database = client.db("doctor_portal_13");
         const serviceCollection = database.collection("services");
         const bookingCollection = database.collection("bookings");
+        const userCollection = database.collection("users");
 
 
         //All API naming convention
@@ -42,6 +63,55 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+
+        // This is api to check whether an user is admin or not
+        app.get('/admin/:email', async(req, res) =>{
+            const email = req.params.email;
+            const query = {email:email};
+            const user = await userCollection.findOne(query);
+            const isAdmin = user.role === 'admin';
+            res.send({admin: isAdmin});
+        })
+
+        //Check specific user by email its already in database or not...if not then insert
+        app.put('/users/:email', async(req, res) =>{
+            const email = req.params.email;
+            const user = req.body;
+            const filter = {email:email};
+            const options = {upsert: true};
+            const updateDoc = {
+                $set:user
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({email:email}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+            res.send({result, token});
+        })
+
+        // Admin api
+        app.put('/users/admin/:email', verifyJWT, async(req, res) =>{
+            const email = req.params.email;
+            const requesterEmail = req.decoded.email;
+            const reqAccount = await userCollection.findOne({email: requesterEmail});
+            if(reqAccount.role === 'admin'){
+                const filter = {email:email};
+                const updateDoc = {
+                    $set:{role: 'admin'}
+                };
+                const result = await userCollection.updateOne(filter, updateDoc);
+                res.send(result);
+            }
+            else{
+                res.status(403).send({message: 'Forbidden'});
+            }
+            
+        })
+
+
+        //Get all users
+        app.get('/user',verifyJWT, async(req, res) =>{
+            const users = await userCollection.find().toArray();
+            res.send(users);
         })
 
         app.get('/available', async(req, res) =>{
@@ -66,12 +136,19 @@ async function run() {
         })
 
         //Get booking for particular patient using email
-        app.get('/booking', async(req, res) =>{
+        app.get('/booking',verifyJWT, async(req, res) =>{
             const patient = req.query.patient;
-            // console.log(patient);
-            const query ={patient:patient};
-            const bookings = await bookingCollection.find(query).toArray();
-            res.send(bookings);
+            const decodedEmail = req.decoded.email;
+            if(patient === decodedEmail){
+                // console.log(patient);
+                const query ={patient:patient};
+                const bookings = await bookingCollection.find(query).toArray();
+                return res.send(bookings);
+            }
+            else{
+                return res.status(403).send({message:'Forbidden Access'});
+            }
+            
         })
 
         //Add Bokking to database
